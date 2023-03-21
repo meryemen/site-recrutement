1016fe84c5fcd6d8177554be2df816026876704b
meryemen <meryemennajibi123@gmail.com>
2023-03-21 21:55:59 +0000

    init

---
+++ controllers/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start(); // Démarrer la session
+
+if(isset($_SESSION['email'])) {
+  // Si l'utilisateur est connecté, déconnectez-le
+  unset($_SESSION['email']);
+  session_destroy();
+  $type_utilisateur=$_SESSION['type_utilisateur'];
+  if($type_utilisateur === 'candidat' ){
+    header("Location: ../pages/offreemploi.php"); // Rediriger vers la page du candidat
+  }else{
+    header("Location: ../pages/dashboardrec.php"); // Rediriger vers la page du recruteur
+  }
+  
+}
+?>
